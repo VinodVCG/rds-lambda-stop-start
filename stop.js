1380d04de215c7e04585724664f80668a6f0f69b
VinodVCG <46479958+VinodVCG@users.noreply.github.com>
2019-02-05 15:45:38 +0530

    Update stop.js
    Removed takins snapshot

--- stop.js
+++ stop.js
@@ -4,8 +4,7 @@ module.exports = (instanceId) => {
     var rds = new AWS.RDS();
     var today = new Date();
     var params = {
-      DBInstanceIdentifier: instanceId, 
-      DBSnapshotIdentifier: `${instanceId}-${today.getDate()}-${today.getMonth()+1}-${today.getFullYear()}-${today.getTime()}`
+      DBInstanceIdentifier: instanceId
     };
     rds.stopDBInstance(params, function(err, data) {
       if (err) console.log(err, err.stack); // an error occurred
